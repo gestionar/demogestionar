68fd017b75c9550a8e0fb7040edacbcc271ab49d
Sebastian Gambogi <sebastian.pablog@gmail.com>
2019-12-26 12:27:34 -0300

    Updates

---
+++ precache-manifest.a6f895a5c4d10e506320018c1ad48761.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15c2f89976dc2448de53aca79491bf40",
+    "url": "/demogestionar/index.html"
+  },
+  {
+    "revision": "5c0def093650f753d955",
+    "url": "/demogestionar/static/css/main.4e3b4756.chunk.css"
+  },
+  {
+    "revision": "d9c3780ea966dff23f43",
+    "url": "/demogestionar/static/js/2.bcde261e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/demogestionar/static/js/2.bcde261e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5c0def093650f753d955",
+    "url": "/demogestionar/static/js/main.9f014d34.chunk.js"
+  },
+  {
+    "revision": "8c41883cb783a9cd88d9",
+    "url": "/demogestionar/static/js/runtime-main.f21c6aab.js"
+  }
+]);
